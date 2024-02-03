5ed9d800358f77b16cc10631f68cf074bf87ef08
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-02-03 22:10:32 +0500

    Add files via upload
    this project is very intresting

---
+++ script.js
@@ -0,0 +1,30 @@
+var question=prompt("Which video do you want to see ?")
+
+var answer=(question)
+if (answer == "Tum Jo Aye Zindagi")
+document.write('<iframe class="a" src="https://www.youtube.com/embed/xxoxHtlNQRk?si=9vzXMd2kcF4Nr231&amp;start=2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+else if (answer == "tum jo aye zindagi")
+document.write('<iframe class="a" src="https://www.youtube.com/embed/xxoxHtlNQRk?si=9vzXMd2kcF4Nr231&amp;start=2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+
+
+var answer=(question)
+if(answer == "Falak Tak")
+document.write('<iframe class="b" src="https://www.youtube.com/embed/rOb-Umam2bY?si=ODjuD9d43zm3tQEf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+else if (answer == "falak tak")
+document.write('<iframe class="b" src="https://www.youtube.com/embed/rOb-Umam2bY?si=ODjuD9d43zm3tQEf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+
+
+var answer=(question)
+if(answer =="Janiye") 
+document.write('<iframe class="C" src="https://www.youtube.com/embed/6fqjDX4a2Nw?si=xN_pUHn3m0wv4dPg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+else if (answer == "janiye")
+document.write('<iframe class="C" src="https://www.youtube.com/embed/6fqjDX4a2Nw?si=xN_pUHn3m0wv4dPg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+
+
+var answer=(question)
+if(answer == "What Jhumka")
+document.write('<iframe class="d" src="https://www.youtube.com/embed/6Gs3Fkf7Mrw?si=iTj5Ha-5y7PfLxI_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+else if (answer =="what jhumka")
+document.write('<iframe class="d" src="https://www.youtube.com/embed/6Gs3Fkf7Mrw?si=iTj5Ha-5y7PfLxI_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+
+
